731db5dcd6a194e8c9a043179946f692aef2b382
Nyanta-23 <muhamadilhan02404@gmail.com>
2025-05-02 10:11:56 +0700

    fix: navbar on app main

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,8 @@
 <section id="navbar" class="container z-1 d-flex justify-content-center">
 
-    <div class="row border-top mt-5 py-2 rounded-5 shadow-navigation bg-light mt-lg bottom-nav mb-3 mx-sm-2">
+    <div class="row border-top mt-5 py-2 rounded-5 bg-light mt-lg bottom-nav mb-3 mx-sm-2">
 
-        <div class="col-sm-12">
+        <div class="col-sm-12 d-flex justify-content-center">
 
             <ul class="z-10 d-flex justify-content-between align-items-center px-0 py-3 gap-1 border-top rounded-top-5 shadow-navigation bg-light bottom-nav w-100 mb-0 pb-4">
                 <li class="list-group-item mx-auto">
